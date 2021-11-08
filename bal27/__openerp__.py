ef1fec4a2c4b078d294911e91cffde3f543ff4a1
32bitPC <61229333+32bitPC@users.noreply.github.com>
2021-11-08 13:26:22 +0700

    Create __openerp__.py

---
+++ bal27/__openerp__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Hello World',
+    'description' : 'Hello World App',
+    'author' : 'Chuong Tran',
+    'depends' : ['base'],
+    'data': [
+        'views/hello_world.xml',
+    ],
+}
